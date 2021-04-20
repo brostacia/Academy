e12d88f1865c66c1bf369874b7941d5d2e250e75
Brostacia <brostacia@gmail.com>
2021-04-20 14:07:13 +0700

    smooth scroll jQuery

--- js/app.js
+++ js/app.js
@@ -11,17 +11,33 @@ $(function() {
 		introH = intro.innerHeight();
 		// обновлять scroll
 		scrollPos = $(this).scrollTop();
-
-		if( scrollPos > introH ) {
-			header.addClass("fixed");
-	 	} else {
-			header.removeClass("fixed");
-	  	}
-
-
 		// console.log(scrollPos);
-
 	});
 
-	
+	function checkScroll(scrollPos, introH) {
+		if( scrollPos > introH ) {
+			 header.addClass("fixed");
+		} else {
+			 header.removeClass("fixed");
+		}
+  }
+
+
+	/* Smooth scroll */
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		let elementId = $(this).data('scroll');
+		//отступ блока от верха 
+		let elementOffset = $(elementId).offset().top;
+
+
+		console.log(elementOffset);
+
+		// 700 миллисек длится скролл
+		$("html, body").animate({
+			scrollTop: elementOffset - 80
+	  }, 700);
+		
+	});
 });
